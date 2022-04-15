cb7204bc059c3718c2357507a7e4e77e5f2f74e7
jrobertunder <88185044+jrobertunder@users.noreply.github.com>
2022-04-14 17:57:54 -0700

    punishment

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,25 @@
 const tableData = data;
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
+function buildTable(data) {
+    tbody.html("");
+    data.forEach((datarow) => { 
+        
+        let row = tbody.append('tr');
+        Object.values(datarow).forEach((val)=> {
+            
+            let cell = row.append("td");
+            cell.text(val);
+        }
+        );
+    });
+}
+function handleclick() {
+    let date = d3.select("#datetime").property("value");
+    let filtereddata = tableData;
+    if (date) {
+        filtereddata = filtereddata.filter(row => row.datetime === date);
+        buildTable(filtereddata);
+    };
+d3.selectall("#filter-btn").on("click", handleclick);
+buildTable(tableData);
